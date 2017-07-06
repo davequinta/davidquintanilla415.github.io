22aa51dbdd21509bac047a172485e6ecb2b977ce
David <david.quinta415@gmail.com>
2017-07-06 13:24:47 -0600

    Initial Commit

---
+++ js/funcionespdf.js
@@ -0,0 +1,160 @@
+function CasoDiscretoPDF(){
+  var pdf = new jsPDF('p', 'pt', 'letter');
+  var y=100; //Variable para llevar posicion en x del texto
+  var x=40;//variable para llevar posicion en y del texto
+  var p=y;
+  pdf.setFontSize(18);
+  pdf.setFont('Courier', 'bold');
+  pdf.setTextColor(0,2,50);
+  pdf.text('Reporte de Minimos Cuadrados - Caso Discreto', x,45)
+  pdf.setFontSize(12);
+  pdf.text('by "SMIRNOV"', x, 60)
+  pdf.setFont('Helvetica', 'Oblique');
+  pdf.setFontSize(12);
+  pdf.setTextColor(0,0,0);
+ 
+  y+=20;
+  pdf.text('Ecuaciones Normales encontradas: ', x ,y)
+  // variable e = numero de ecuaciones normales
+  var e = 4;
+  for (var i = 0; i < e; i++) {
+    pdf.text('Ecuación ' + i + ': ', 290, y)
+    pdf.text('Q1x + Q2x^2 - Q3x^3', 355, y)
+    y+=20;
+    }
+  y+=20;
+  pdf.text('Coeficientes encontradas: ', x ,y)
+// variable f = numero de incognitas encontradas
+  var f = 4;
+  for (var i = 0; i < f; i++) {
+    pdf.text('Q ' + i + '= ', 290, y)
+    pdf.text(i + '', 315, y)
+    y+=20;
+    }
+  y+=20;
+  pdf.text('Polinomio generado mediante Minimos Cuadrados: ', x, y)
+  pdf.setFont('Courier', 'bold');
+  pdf.setFontSize(15)
+  y+=30;
+  var polinomio='0.2558x^5 + 2.3x^3 + 1.5x^2 -0.365'; // Llenar variable con respuesta XD
+  pdf.text(polinomio, x, y)
+  y+=30;
+  pdf.setFont('Helvetica', 'Oblique');
+  pdf.setFontSize(12);
+  pdf.text('A continuación presentamos una tabla con los valores interpolados con nuestro polinomio: ', x, y)
+  y+=30;
+  x+=165;
+//pdf.rect(x,y, 525,200);
+// Creacion Tabla
+  pdf.setLineWidth(1);
+  pdf.setDrawColor(0,2,50);
+  pdf.line(x,y,x+200,y);//Linea superior de la tabla
+  pdf.line(x,y+20,x+200,y+20);//Linea inferior de los titulos de la tabla
+  pdf.line(x,y,x,y+20);// linea lateral izquiera de los titulos de la tabla
+  pdf.line(x+200,y,x+200,y+20);//linea lateral derecha de los titulos de la tabla
+  pdf.line(x+100,y,x+100,y+20);//linea divisora de los titulos de la tabla
+  pdf.text('valor en x', x+10, y+16);
+  pdf.text('f(x)', x+110, y+16);
+  y+=20;
+  var inter=5;//variable que tiene el array de valores a interpolar
+  var eva; // variable que tiene el array de los valores ya interpolados
+  var reales; // variable que tiene el array de valores evaluados en funcion original
+  for (var i = 0; i < inter; i++) {
+    pdf.line(x,y,x,y+20);//linea lateral derecha
+    pdf.line(x+200,y,x+200,y+20);//linea lateral izquierda
+    pdf.line(x+100,y,x+100,y+20);//linea divisora
+    pdf.text('valor '+i, x+10, y+16);
+    pdf.text('valor evaluado '+i, x+110, y+16);
+    y+=20;
+  }
+  pdf.line(x,y,x+200,y);//linea inferior de la tabla
+
+  pdf.save('Smirnov');
+}
+function CasoContinuoPDF(){
+  var pdf = new jsPDF('p', 'pt', 'letter');
+  var y=100; //Variable para llevar posicion en x del texto
+  var x=40;//variable para llevar posicion en y del texto
+  var p=y;
+  pdf.setFontSize(18);
+  pdf.setFont('Courier', 'bold');
+  pdf.setTextColor(0,2,50);
+  pdf.text('Reporte de Minimos Cuadrados - Caso Continuo', x,45)
+  pdf.setFontSize(12);
+  pdf.text('by "SMIRNOV"', x, 60)
+  pdf.setFont('Helvetica', 'Oblique');
+  pdf.setFontSize(12);
+  pdf.setTextColor(0,0,0);
+  pdf.text('Función ingresada por el usuario: ', x, y)
+  //variable que almacena la funcion
+  var fun='f(x)= cos(5*x)+sin^2(6*x)+x'
+  pdf.text(fun, 290, y);
+  y+=40;
+  pdf.text('Intervalo ingresado por el usuario:', x, y)
+  var intervalo='[ 1 , 6 ]';
+  pdf.text(intervalo,x+290, y )
+  y+=40;
+  pdf.text('Ecuaciones Normales encontradas: ', x ,y)
+  // variable e = numero de ecuaciones normales
+  var e = 4;
+  for (var i = 0; i < e; i++) {
+    pdf.text('Ecuación ' + i + ': ', 290, y)
+    pdf.text('Q1x + Q2x^2 - Q3x^3', 355, y)
+    y+=20;
+    }
+  y+=20;
+  pdf.text('Coeficientes encontradas: ', x ,y)
+  // variable f = numero de incognitas encontradas
+  var f = 4;
+  for (var i = 0; i < f; i++) {
+    pdf.text('Q ' + i + '= ', 290, y)
+    pdf.text(i + '', 315, y)
+    y+=20;
+    }
+  y+=20;
+  pdf.text('Polinomio generado mediante Minimos Cuadrados: ', x, y)
+  pdf.setFont('Courier', 'bold');
+  pdf.setFontSize(15)
+  y+=30;
+  var polinomio='0.2558x^5 + 2.3x^3 + 1.5x^2 -0.365'; // Llenar variable con respuesta XD
+  pdf.text(polinomio, x, y)
+  y+=30;
+  pdf.setFont('Helvetica', 'Oblique');
+  pdf.setFontSize(12);
+  pdf.text('A continuación presentamos una tabla con los valores interpolados con nuestro polinomio: ', x, y)
+  y+=30;
+  x+=75;
+  // Creacion de la tabla
+  pdf.setLineWidth(1);
+  pdf.setDrawColor(0,2,50);
+  pdf.line(x,y,x+400,y);//linea horizontal superior de la fila de titulos
+  pdf.line(x,y+20,x+400,y+20);//linea horizontal inferior de la fila de titulos
+  pdf.line(x,y,x,y+20);//linea lateral izquierda de la fila de titulos
+  pdf.line(x+200,y,x+200,y+20);// linea divisora central de la fila de titulos
+  pdf.line(x+100,y,x+100,y+20);//linea divisora izquierda de la fila de titulos
+  pdf.line(x+300,y,x+300,y+20);//linea divisora derecha de la fila de titulos
+  pdf.line(x+400,y,x+400,y+20);//linea lateral derecha de la fila de titulos
+  pdf.text('valor en x', x+10, y+16);
+  pdf.text('f(x)', x+110, y+16);
+  pdf.text('valor real', x+210, y+16);
+  pdf.text('ERP' , x+310,y+16);
+  y+=20;
+  var inter=5;//variable que tiene el array de valores a interpolar
+  var eva; // variable que tiene el array de los valores ya interpolados
+  var real;//variable que contiene el array de valores evaluados en la funcion original
+  for (var i = 0; i < inter; i++) {
+    pdf.line(x,y,x,y+20);//linea lateral izquierda
+    pdf.line(x+200,y,x+200,y+20);//linea divisora central
+    pdf.line(x+100,y,x+100,y+20);//linea divisora izquierda
+    pdf.line(x+300,y,x+300,y+20);//linea divisora derecha
+    pdf.line(x+400,y,x+400,y+20);//linea lateral derecha
+    pdf.text('valor '+i /*inter[i]*/, x+10, y+16);
+    pdf.text('valor evaluado '+i /*eva[i]*/, x+110, y+16);
+    pdf.text('valor real ' +i /*real[i]*/, x+210, y+16);
+    pdf.text('ERP '+i /* ((real[i]-eva[i])/real[i])*100 + '%' */, x+310,y+16);
+    y+=20;
+  }
+  pdf.line(x,y,x+400,y);//Linea inferior de la tabla
+
+  pdf.save('Smirnov');
+}
---
+++ least_squares.js
@@ -0,0 +1,469 @@
+function calculate() {
+     
+    //Numero de datos
+    var n=10;
+    //Grado del polinimo
+    var m=1;
+    
+    //document.write('Ejercicio de grado ' + m + '<br><br>');
+    //Puntos
+     var x = [4.0,4.2,4.5,4.7,5.1,5.5,5.9,6.3,6.8,7.1];
+     var y = [102.56,113.18,130.11,142.05,167.53,195.14,224.87,256.73,299.50,326.72];
+    
+   
+    //var x= [1,2,5,15,25,30,35,40];
+    //var y= [99,95,85,55,30,24,30,15];
+    
+	
+       
+    var x1 = [1,1.2,1.5,2,3,3.7,4,4.5];
+     var y1 = [3,3.4,5,2,4.1,5,7,6.5];
+    	
+	
+    
+    document.getElementById('dpol').innerHTML=DiscPol(n,m,x,y);
+    document.getElementById('dexp').innerHTML=DiscExpo(n,x,y);
+    document.getElementById('dpot').innerHTML=DiscPoten(n,x,y);
+    document.getElementById('dlog').innerHTML=DiscLog(8,x1,y1);
+    
+    
+   document.getElementById('cpol').innerHTML=ContiPol(1,'x^3','0','2');
+   
+    
+  
+
+    console.log("Testeo");
+ 
+    
+    plotDisc(parOrd(x,y),parOrd(x,evalFunc('((-194.1382407320934*x^0)+(72.0845176953962*x^1))',x)));
+
+    
+    plotCon(parOrd(x,y),parOrd(x,y));
+
+
+    console.log("Funcion");
+    console.log(evalCn('1/x',1,3));
+    console.log(evalCn('((1.1389788818415778*x^0)+(-0.294639317240115*x^1))',1,3));
+    
+    /*
+    var x7= [0,1,2,3,4,5,6,7,8,9]
+     plotCon(parOrd(x7,evalFunc('1/x',x7)),parOrd(x7,evalFunc('((1.1389788818415778*x^0)+(-0.294639317240115*x^1))',x7)));
+    */
+    var x7 = [1,2,3];
+    plotCon(parOrd(x7,evalCn('1/x',1,3)),parOrd(x7,evalCn('((1.1389788818415778*x^0)+(-0.294639317240115*x^1))',1,3)))
+    
+          
+ 
+    
+
+   
+   
+}
+
+
+
+//Funcion para graficar Discretos
+function evalCn(cad,a,b){
+    var node2 = math.parse(cad);
+    var code2 = node2.compile();
+    
+    var evalpoints = new Array();
+    for(var i=a;i<=b;i++){
+         var epoint =i;
+        var scope = {
+          x: epoint,
+        };
+        evalpoints.push(code2.eval(scope));
+        
+    }
+    
+    return evalpoints;
+}
+
+
+
+function plotDef(){
+
+}
+function evalFunc(cad,x){
+    var node2 = math.parse(cad);
+    var code2 = node2.compile();
+                      
+
+    var evalpoints = new Array();
+    for(var i=0;i<x.length;i++){
+         var epoint =x[i];
+        var scope = {
+          x: epoint,
+        };
+        evalpoints.push(code2.eval(scope));
+        
+    }
+    
+    return evalpoints;
+}
+
+
+
+function parOrd(x,y){
+    var xy = createMatrix(x.length);
+
+    for(var i =0; i<x.length;i++){
+        for(var j=0;j<1;j++){
+            xy[i].push(x[i+j]);
+            xy[i].push(y[i+j]);
+        }
+    }
+    
+    return xy;
+    
+}
+
+function plotCon (par,ypar){
+    
+    var xy = JSON.parse(JSON.stringify(par));
+    var graf = JSON.parse(JSON.stringify(ypar));
+
+
+     Highcharts.chart('container2', {
+    xAxis: {
+        min: -0.5,
+        max: 10
+    },
+    yAxis: {
+        min: 0
+    },
+    title: {
+        text: 'Scatter plot with regression line'
+    },
+    series: [{
+        type: 'line',
+        name: 'Regression Line',
+        data: graf,
+        marker: {
+            enabled: false
+        },
+        states: {
+            hover: {
+                lineWidth: 0
+            }
+        },
+        enableMouseTracking: false
+    }, {
+        type: 'line',
+        name: 'Regression Line',
+        data: xy,
+        marker: {
+            enabled: false
+        },
+        states: {
+            hover: {
+                lineWidth: 0
+            }
+        },
+        enableMouseTracking: false
+    }]
+});
+
+}
+
+function plotDisc(par,ypar){
+    
+    var xy = JSON.parse(JSON.stringify(par));
+    var graf = JSON.parse(JSON.stringify(ypar));
+
+
+     Highcharts.chart('container', {
+    xAxis: {
+        min: -0.5,
+        max: 10
+    },
+    yAxis: {
+        min: 0
+    },
+    title: {
+        text: 'Scatter plot with regression line'
+    },
+    series: [{
+        type: 'line',
+        name: 'Regression Line',
+        data: graf,
+        marker: {
+            enabled: false
+        },
+        states: {
+            hover: {
+                lineWidth: 0
+            }
+        },
+        enableMouseTracking: false
+    }, {
+        type: 'scatter',
+        name: 'Observations',
+        data: xy,
+        marker: {
+            radius: 4
+        }
+    }]
+});
+
+}
+
+
+// Funcion para pares ordenados
+
+
+//Funcion de integral definida
+function d_integral(fun, li, ls){
+    var StrIntg = 'integrate(' + fun + ',x,'+li+','+ls+')';
+    return math.eval(StrIntg);
+    
+}
+
+
+
+/*
+Crea array bidimensional que no sé por qué 
+no salía con var array = [[]]; xD
+*/
+function createMatrix(rows){
+    var matrix = new Array();
+    for(var i=0;i<rows;i++){
+        matrix.push([]);
+    }
+    return matrix;
+}
+
+
+//************ METODO DISCRETO****/
+//Array sumatorias de "x"; n numero de datos, m grado del polinomio deseado
+function fillCoef1(n, m, x){
+    var aux;
+    var coef1 = new Array();
+     for(var i=0;i<(m*2)+1;i++){
+        aux=0;
+        for(var k=0;k<n;k++){
+            aux+=Math.pow(x[k],i);   
+        }
+        coef1.push(aux);
+    }
+    
+    return coef1;
+}
+
+//Matriz coeficientes de Ecc Normales
+function fillMatrix(m, array){
+    var ecc = createMatrix(m+1);
+    for (var i=0;i<m+1;i++){
+        //document.wr`ite(i);    
+        for(var j=0;j<m+1;j++){
+            //document.write(matrix.length);
+            //matrix[i][j]=coef1[(i+j)];
+            ecc[i].push(array[i+j]);   
+            //document.write('<br>'+'>>'+(i+ j));
+        }
+    }
+    return ecc;
+}
+
+/*Array coeficientes a lo que están igualadas las eccuaciones
+Sum(y), sum(xy), etc
+*/
+function fillCoef2(n,m,x,y){
+    var aux;
+    var coef2 = new Array();
+    
+    for(var k=0; k<m+1; k++){
+		aux=0;
+		for(var i=0; i < n; i++){
+            aux+=(Math.pow(x[i], k))*y[i];
+        }
+        coef2.push(aux);	
+	}  
+    return coef2;
+   
+}
+
+/*Resuelve el sistema de eccuaciones con Math.usolve(parm1,parm2):
+Parm1 es una matrix, parm2 un array
+*/
+function solveEcc(matrix, array){
+    return numeric.solve(matrix,array);
+}
+
+
+/**GENERADOR DE ECUACIONES **/
+
+//Crea polinomio de grado m
+function createPol(values,m){
+    var fun = '';
+    for(var i=0;i<m+1;i++){
+        fun+= ('('+values[i] + "*x^" +i+')');
+        if(i<m){
+            fun+='+';
+        }
+    }
+    return '('+fun+')';
+}
+
+function createExpFun(values){
+    return '('+ math.exp(values[0])+'*(e^('+values[1]+'*x)'+'))';
+}
+
+function createPotFun(values){
+    return '('+ math.exp(values[0])+'*x^'+values[1]+')';
+    
+}
+function createLogFun(values){
+    return '('+ math.exp(values[0])+'*ln(x)+'+values[1]+')';  
+}
+
+
+/**FUNCIONES PARA LOS METODOS **/
+
+//Funcion para caso discreto polinomial
+function DiscPol(n,m,x,y){
+    var coef1 = fillCoef1(n,m,x);
+    //Matrix
+    var matrix = fillMatrix(m,coef1);
+    //Arreglo coeficientes "xy"
+    var coef2 = fillCoef2(n,m,x,y);
+    //document.write(coef2);
+    //Valores de las incognitas
+    var values = solveEcc(matrix,coef2); 
+    return createPol(values,m);    
+}
+function DiscExpo(n,x,y){
+    var coef1 = fillCoef1(n,1,x);
+    var matrix = fillMatrix(1,coef1);
+    var coef2 = fillCoef2(n,1,x,math.log(y));
+    var values = solveEcc(matrix,coef2); 
+    //Funcion --
+    return createExpFun(values);
+    
+}
+
+
+function DiscPoten(n,x,y){
+    
+    var coef1 = fillCoef1(n,1,math.log(x));
+    var matrix = fillMatrix(1,coef1);
+    var coef2 = fillCoef2(n,1,math.log(x),math.log(y));
+    var values = solveEcc(matrix,coef2); 
+    //Funcion --
+    return createPotFun(values);
+    
+}
+
+function DiscLog(n,x,y){
+    var coef1 = fillCoef2(n,1,math.log(x),y);
+    var coef2 = fillCoef1(n,1,math.log(y));
+    var coef3 = fillCoef1(n,2,math.log(x));
+    var coef4 = fillCoef1(n,1,math.log(x));   
+    var p1 = coef1[1];
+    var p2 = coef1[0];
+    var p3 = coef2[1];
+    var p4 = coef3[2];
+    var p5 = coef4[1];
+    var a = (p1-(p2/n)*p5)/(p4-(p5/n)*p5);
+
+    var b = (p2/n) - (a * (p5/n));
+    var values = [a,b];
+    return createLogFun(values);
+    
+}
+
+function DiscPer(){
+  
+}
+
+/*******  FUNCIONES PARA CONTINUOS *********/
+
+function fillCoef1Conti(m){
+    var coef1 = new Array();
+     for(var i=0;i<(m*2)+1;i++){
+        coef1.push('x^'+i);
+    }
+    
+    return coef1;
+}
+
+
+function fillCoef1ContiPoten(m){
+    var coef1 = new Array();
+     for(var i=0;i<(m*2)+1;i++){
+        coef1.push('log('+'x^'+i+')');
+    }
+    
+    return coef1;
+}
+
+
+function fillMatrixConti(m, array,li,ls){
+    var aux;
+    var ecc = createMatrix(m+1);
+    for (var i=0;i<m+1;i++){
+        //document.wr`ite(i);    
+        for(var j=0;j<m+1;j++){
+            aux=0;
+            aux = d_integral(array[i+j],li,ls);
+    
+            //document.write(matrix.length);
+            //matrix[i][j]=coef1[(i+j)];
+            ecc[i].push(aux);   
+            //document.write('<br>'+'>>'+(i+ j));
+        }
+    }
+    return ecc;
+}
+
+
+
+
+
+function fillCoef2Conti(fun,m,li,ls,x){
+   
+    var coef2 = new Array();
+    for(var k=0; k<m+1; k++){
+		aux=0;
+		aux = d_integral((fun+'*'+x[k]),li,ls);
+        coef2.push(aux);	
+	}  
+    return coef2;
+   
+}
+
+/*** METODOS CONTINUOS ***/
+function ContiPol(m,fun,li,ls){
+    var coef1 = fillCoef1Conti(m);
+    
+   console.log(coef1);
+    var cmatrix = fillMatrixConti(m,coef1,li,ls);
+    console.log("Valores");
+    console.log(cmatrix);
+    var yun = fun;
+    var ccoef2 = fillCoef2Conti(yun,m,li,ls,coef1); 
+    console.log("Igualados");
+    console.log(ccoef2);
+    
+    var incognitas = solveEcc(cmatrix,ccoef2); 
+    console.log("Incognitas!");
+    console.log(incognitas);
+    
+     return createPol(incognitas,m); 
+}
+function ContiExpo(){
+    
+}
+
+function ContiPoten(){
+    
+}
+
+function ContiLog(){
+    
+}
+
+
+
+
